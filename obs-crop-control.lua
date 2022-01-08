8e80b3cf0972f97874bd0468c4b138433ce804f2
Ralf S. Engelschall <rse@engelschall.com>
2022-01-08 11:42:44 +0100

    allow PREVIEW and PROGRAM to be used independently

--- obs-crop-control.lua
+++ obs-crop-control.lua
@@ -31,7 +31,7 @@ local function recall (control, define)
     event.modifiers        = 0
     event.native_scancode  = 0
     event.native_modifiers = 0
-    event.text             = "" .. define
+    event.text             = define
     obs.obs_source_send_key_click(controlSource, event, false)
     obs.obs_source_send_key_click(controlSource, event, true)
 
@@ -54,9 +54,9 @@ end
 --  hook: provide default settings (initialization before create)
 info.get_defaults = function (settings)
     --  provide default values
-    obs.obs_data_set_default_bool(settings, "program", true)
     obs.obs_data_set_default_string(settings, "control", "")
-    obs.obs_data_set_default_int(settings, "define", 1)
+    obs.obs_data_set_default_string(settings, "define1", "none")
+    obs.obs_data_set_default_string(settings, "define2", "none")
 end
 
 --  hook: create filter context
@@ -71,7 +71,8 @@ info.create = function (_settings, source)
     filter.cfg = {
         program = true,
         control = "",
-        define  = 1
+        define1 = "none",
+        define2 = "none"
     }
     obs.script_log(obs.LOG_INFO, string.format("hook: create: filter name: \"%s\"", filter.name))
     return filter
@@ -87,39 +88,79 @@ end
 
 --  hook: after loading settings
 info.load = function (filter, settings)
-    filter.cfg.program = obs.obs_data_get_bool(settings, "program")
+    --  take current parameters
     filter.cfg.control = obs.obs_data_get_string(settings, "control")
-    filter.cfg.define  = obs.obs_data_get_int(settings, "define")
+    filter.cfg.define1 = obs.obs_data_get_string(settings, "define1")
+    filter.cfg.define2 = obs.obs_data_get_string(settings, "define2")
+
+    --  hook: activate (preview)
+    obs.obs_frontend_add_event_callback(function (ev)
+        if ev == obs.OBS_FRONTEND_EVENT_PREVIEW_SCENE_CHANGED then
+            if filter.parent ~= nil then
+                local sceneSource      = obs.obs_frontend_get_current_preview_scene()
+                local sceneSourceName  = obs.obs_source_get_name(sceneSource)
+                local filterSourceName = obs.obs_source_get_name(filter.parent)
+                local scene            = obs.obs_scene_from_source(sceneSource)
+                local sceneItem        = obs.obs_scene_find_source_recursive(scene, filterSourceName)
+                obs.obs_source_release(sceneSource)
+                if sceneItem then
+                    if filter.cfg.define1 ~= "none" then
+                        obs.script_log(obs.LOG_INFO, string.format(
+                            "hook: scene \"%s\" with filter source \"%s\" is in PREVIEW now -- reacting",
+                            sceneSourceName, filterSourceName))
+                        recall(filter.cfg.control, filter.cfg.define1)
+                    end
+                end
+            end
+        end
+    end)
 end
 
 --  hook: provide filter properties (for dialog)
 info.get_properties = function (_filter)
     --  create properties
     local props = obs.obs_properties_create()
-    obs.obs_properties_add_bool(props, "program", "Activate define when scene becomes active in Program only")
     obs.obs_properties_add_text(props, "control", "Source Name of Control UI:", obs.OBS_TEXT_DEFAULT)
-    obs.obs_properties_add_int(props, "define", "Recall Crop Control Define (1-9):", 1, 9, 1)
+    local function addList (prop)
+        obs.obs_property_list_add_string(prop, "none", "none")
+        obs.obs_property_list_add_string(prop, "1", "1")
+        obs.obs_property_list_add_string(prop, "2", "2")
+        obs.obs_property_list_add_string(prop, "3", "3")
+        obs.obs_property_list_add_string(prop, "4", "4")
+        obs.obs_property_list_add_string(prop, "5", "5")
+        obs.obs_property_list_add_string(prop, "6", "6")
+        obs.obs_property_list_add_string(prop, "7", "7")
+        obs.obs_property_list_add_string(prop, "8", "8")
+        obs.obs_property_list_add_string(prop, "9", "9")
+    end
+    local define1 = obs.obs_properties_add_list(props, "define1", "Recall Define on Preview:",
+        obs.OBS_COMBO_TYPE_LIST, obs.OBS_COMBO_FORMAT_STRING)
+    local define2 = obs.obs_properties_add_list(props, "define2", "Recall Define on Program:",
+        obs.OBS_COMBO_TYPE_LIST, obs.OBS_COMBO_FORMAT_STRING)
+    addList(define1)
+    addList(define2)
     return props
 end
 
 --  hook: react on filter property update (during dialog)
 info.update = function (filter, settings)
-    filter.cfg.program = obs.obs_data_get_string(settings, "program")
     filter.cfg.control = obs.obs_data_get_string(settings, "control")
-    filter.cfg.define  = obs.obs_data_get_int(settings, "define")
+    filter.cfg.define1 = obs.obs_data_get_string(settings, "define1")
+    filter.cfg.define2 = obs.obs_data_get_string(settings, "define2")
 end
 
 --  hook: activate (program)
 info.activate = function (filter)
-    if filter.cfg.program then
-        recall(filter.cfg.control, filter.cfg.define)
-    end
-end
-
---  hook: show (anywhere)
-info.show = function (filter)
-    if not filter.cfg.program then
-        recall(filter.cfg.control, filter.cfg.define)
+    if filter.cfg.define2 ~= "none" then
+        if filter.parent ~= nil then
+            local sceneSource      = obs.obs_frontend_get_current_scene()
+            local sceneSourceName  = obs.obs_source_get_name(sceneSource)
+            local filterSourceName = obs.obs_source_get_name(filter.parent)
+            obs.script_log(obs.LOG_INFO, string.format(
+                "hook: scene \"%s\" with filter source \"%s\" is in PROGRAM now -- reacting",
+                sceneSourceName, filterSourceName))
+            recall(filter.cfg.control, filter.cfg.define2)
+        end
     end
 end
 
@@ -157,10 +198,9 @@ function script_description ()
         href="https://spdx.org/licenses/GPL-3.0-only.html">GPL 3.0 license</a>
 
         <p>
-        <b>Recall a Crop Control Define filter for sources. This is intended
-        to allow OBS Studio to force Crop Control to recall a crop
-        define in case the source becomes visible (aka shown) in any
-        display or visible (aka active) in the Program.</b>
+        <b>Recall a Crop Control define if the scene with the source of the filter
+        <i>Recall Crop Control Define</i> becomes visible in the PREVIEW (for
+        enabled studio mode only) and/or in the PROGRAM.</b>
         </p>
     ]]
 end
